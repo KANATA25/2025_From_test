8db2b9c5ee80a6d6d3b9a385b7906c5aab76ae38
SAME学校用\kamat <rourutianli@gmail.com>
2025-10-24 15:36:56 +0900

    練習問題1,2

--- From_Test/From_Test/Form1.cs
+++ From_Test/From_Test/Form1.cs
@@ -14,7 +14,45 @@ namespace From_Test
     {
         public Form1()
         {
+
+           
             InitializeComponent();
+            for (int i = 1; i<= 6; i++)
+            {
+                for (int s = 1; s <= 4; s++)
+                {
+                    TestButton testButton = new TestButton();
+
+                    //ボタンの位置
+                    testButton.Location = new Point(100 * i, 50 * s);
+
+
+                    //サイズ設定
+                    testButton.Size = new Size(100, 100);
+
+                    testButton.Text = "押すなよその二";
+
+                    //コントロールにボタンの追加
+                    Controls.Add(testButton);
+
+                }
+
+            }
+            
+            
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("押すなって言ったのに。まぁ、別に何にも無いけどね");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
